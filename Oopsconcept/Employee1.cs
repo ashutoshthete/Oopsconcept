db5d748ce82d283c5bff41aa376dd9372617f5de
ashutoshthete <ashutoshthete24@gmail.com>
2022-09-17 07:31:37 +0530

    sep 17

---
+++ Oopsconcept/Employee1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oopsconcept
+{
+    class Employee1
+    {
+        string name;
+        int eid;
+        int mgid;
+        string mname;
+
+        public Employee1(string name, int eid, int mgid, string mname)
+        {
+            this.name = name;
+            this.eid = eid;
+            this.mgid = mgid;
+            this.mname = mname;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public int Eid { get => eid; set => eid = value; }
+        public int Mgid { get => mgid; set => mgid = value; }
+        public string Mname { get => mname; set => mname = value; }
+
+        public void showdata()
+        {
+            Console.WriteLine("Employee name : "+Name+"\nEmployee id : "+Eid+"\nManager id : "+Mgid+"\nMangaer name : "+Mname);
+        }
+    }
+    class testemp
+    {
+        static void Main(string[] args)
+        {
+
+            Employee1[] emp = new Employee1[5];
+
+            emp[0] = new Employee1("ashutosh", 255, 1745, "atul\n");
+            emp[1] = new Employee1("arivind", 256, 1745, "atul\n");
+            emp[2] = new Employee1("gauri", 257, 1745, "atul\n");
+            emp[3] = new Employee1("amit", 258, 1745, "atul\n");
+            emp[4] = new Employee1("anup", 259, 1745, "atul\n");
+
+
+            Console.WriteLine("Employee 1:");
+            emp[0].showdata();
+            Console.WriteLine("Employee 2:");
+            emp[1].showdata();
+            Console.WriteLine("Employee 3:");
+            emp[2].showdata();
+            Console.WriteLine("Employee 4:");
+            emp[3].showdata();
+            Console.WriteLine("Employee 5:");
+            emp[4].showdata();
+        }
+    }
+}
---
+++ Oopsconcept/TqTestsepno1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oopsconcept
+{
+    class TqTestsepno1
+    {
+        public static void findpair(int[]arr,int sumtobe)
+        {
+            for(int i = 0; i < arr.Length - 1; i++)
+            {
+                for(int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] + arr[j] == sumtobe)
+                    {
+                        Console.WriteLine("Pair found : ("+arr[i]+"),("+arr[j]+")");
+                    }
+                   
+
+                }
+
+            }
+            
+
+        }
+        public static void Main(string[] args)
+        {
+            int[] arr = { 5, 4, 6, 8, 9, 7, 25, 6, 44, 25, 38, 44 };
+            int sumtobe = 15;
+            findpair(arr, sumtobe);
+
+        }
+    }
+}
