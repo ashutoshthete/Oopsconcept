a07897edfd1f307d5167561ac3d6954a1509d40d
ashutoshthete <ashutoshthete24@gmail.com>
2022-10-07 08:18:39 +0530

    oct 7

---
+++ Oopsconcept/LongSubArr.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oopsconcept
+{
+    class LongSubArr
+    {
+        static int maxSubarrLength(int[] arr)
+        {
+            int maxlen = 0;
+            for(int i = 0; i < arr.Length; i++)
+            {
+                int zeros = 0;
+                int ones = 0;
+                for(int j = 0; j < arr.Length; j++)
+                {
+                    if (arr[j] == 0)
+                    {
+                        zeros++;
+                    }
+                    else
+                    {
+                        ones++;
+                    }
+                    if (zeros == ones)
+                    {
+                        maxlen = Math.Max(maxlen, j - i + 1);
+                    }
+                }
+            }
+            return maxlen;
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1 };
+            int result = maxSubarrLength(arr);
+            Console.WriteLine(result);
+        }
+    }
+}
